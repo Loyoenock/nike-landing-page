7fd5ce1756bce03aa2fdb7c2f2593bb2520b9080
loyoenock <loyoenock@gmail.com>
2024-07-04 20:29:08 +0300

    Set overflow properties to allow the ShoeCard components to overflow the hero section into the section below.

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
   return (
     <section 
       id='home'
-      className='w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container overflow-hidden'
+      className='w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container relative'
     >
       <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:px-6 pt-28'>
         <p className='text-xl font-montserrat text-coral-red'>Our Festive Collection</p>
@@ -36,15 +36,15 @@ const Hero = () => {
           ))}
         </div>
       </div>
-      <div className='relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-20 bg-primary bg-hero bg-cover bg-center overflow-hidden'>
+      <div className='relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-20 bg-primary bg-hero bg-cover bg-center'>
         <img 
           src={bigShoeImg}
           alt='Shoe collection'
-          width={510}
-          height={400}
+          width={610}
+          height={500}
           className='object-contain relative z-10 max-sm:w-full max-sm:h-auto'
         />
-        <div className='flex sm:gap-6 gap-4 absolute bottom-0 sm:bottom-5 sm:left-10 left-6 right-6'>
+        <div className='flex sm:gap-6 gap-4 absolute -bottom-16 sm:left-[10%] max-sm:left-6 max-sm:right-6'>
           {shoes.map((shoe) => (
             <div key={shoe}>
               <ShoeCard 
